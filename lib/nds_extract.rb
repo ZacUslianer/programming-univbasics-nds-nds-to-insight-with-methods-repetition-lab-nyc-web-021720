b9c6ec157376afa682b5ce472562735c64f3de25
Zac Uslianer <zacuslianer@gmail.com>
2020-02-06 18:08:16 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -39,9 +39,9 @@ def total_gross(source)
   row_index = 0 
   while row_index < source.length
   grand_total += directors_totals(source)
-  name_index = 0 
-    while
-  row_index += 1
+  binding.pry
+   row_index += 1
+ end
   # Write this implementation
   #
   # Should use methods:
